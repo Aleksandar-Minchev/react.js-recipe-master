e55cf619ae455435c4e4bfbf624281ef8afef1b0
Aleksandar Minchev <aminchev1988@gmail.com>
2025-03-21 21:58:25 +0200

    Add Home Page View

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,37 @@
 import { Routes, Route } from 'react-router';
 
-import About from './components/About';
 import Catalog from './components/Catalog';
-import Contacts from './components/Contacts';
 import Details from './components/Details';
 import Login from './components/Login';
 import Register from './components/Register';
 import Header from './components/header/Header';
+import Home from './components/Home';
+import Contacts from './components/Contacts';
+import About from './components/About';
+import Footer from './components/Footer/Footer';
 
 import './App.css'
 
 function App() {
 
   return (
-    <>
+    <div id="box">
         <Header/>
 
-        <Routes>
+        <main id="main-content">
+          <Routes>
+            <Route path="/" element={<Home />}/>
             <Route path="/login" element={<Login />}/>
-            <Route path="/about" element={<About />}/>
             <Route path="/contacts" element={<Contacts />}/>
-            <Route path="/Catalog" element={<Catalog />}/>
+            <Route path="/about" element={<About />}/>
+            <Route path="/recipes" element={<Catalog />}/>
             <Route path="/register" element={<Register />}/>       
-        </Routes>
-    </>
-    
+            <Route path="/details" element={<Details />}/>       
+          </Routes>
+        </main>
+
+        <Footer/>
+    </div>    
   )
 }
 
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -2,41 +2,41 @@ import { Link } from "react-router";
 
 export default function Header() {
     return(
-        <>
-        <header>
-        <nav>
-        <ul >
-          <li >
-            <Link to="/about">
-              <button>About</button>
-            </Link>
-          </li>
-          <li >
-            <Link to="/contacts">
-              <button>Contacts</button>
-            </Link>
-          </li>
-          <li >
-            <Link to="/register">
-              <button>Register</button>
-            </Link>
-          </li>
-          <li >
-            <Link to="/login">
-              <button>Login</button>
-            </Link>
-          </li>
-          <li >
-            <Link to="/create">
-              <button>Create</button>
-            </Link>
-          </li>
-          <li >
-            <button>Logout</button>
-          </li>
-            </ul>
-        </nav>
-        </header>
-        </>
+      <>
+      <header>
+      <nav>
+      <ul >
+        <li >
+          <Link to="/about">
+            <button>About</button>
+          </Link>
+        </li>
+        <li >
+          <Link to="/contacts">
+            <button>Contacts</button>
+          </Link>
+        </li>
+        <li >
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        </li>
+        <li >
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        </li>
+        <li >
+          <Link to="/create">
+            <button>Create</button>
+          </Link>
+        </li>
+        <li >
+          <button>Logout</button>
+        </li>
+          </ul>
+      </nav>
+      </header>
+      </>
     );
 }
