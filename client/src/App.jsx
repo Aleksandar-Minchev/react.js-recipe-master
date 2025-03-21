735667b98949922524fb5725550110cc2a818230
Aleksandar Minchev <aminchev1988@gmail.com>
2025-03-21 12:57:38 +0200

    Adding Contacts Page View

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import About from './components/About';
+import Catalog from './components/Catalog';
+import Contacts from './components/Contacts';
 
 function App() {
 
   return (
     <>
         <About />
+        <Contacts />
   )
 }
 
---
+++ client/src/components/Contacts.jsx
@@ -0,0 +1,13 @@
+export default function Contacts() {
+    return(
+        <>
+        <h1>Contact Us</h1>
+        <p>If you have any questions, feel free to reach out to us:</p>
+        <ul>
+        <li>Email: contact@ourplatform.com</li>
+        <li>Phone: +1-800-123-4567</li>
+        <li>Address: 123 Platform St, City, Country</li>
+        </ul>
+        </>
+    );
+}
